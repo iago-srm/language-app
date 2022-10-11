4dbeeeb3a4420ca8a9b786d3c11f71f3a8b446e1
Iago Soriano <iago.monteiro@usp.br>
2022-10-11 14:28:54 -0300

    domain api migrations

--- packages/web-api/prisma/migrations/20220905182028_drop_last_version/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `lastVersion` on the `Activity` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Activity" DROP COLUMN "lastVersion";
