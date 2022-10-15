99b2cd1a7a1b2f9a539a8713e0ad7102adf7af3a
Iago Soriano <iago.monteiro@usp.br>
2022-10-15 16:22:38 -0300

    console.log added

--- packages/common-platform/src/platform/services/aws-sqs.ts
+++ packages/common-platform/src/platform/services/aws-sqs.ts
@@ -15,6 +15,7 @@ export class SQSService implements IQueueService {
 
     const sendMessage = promisify(this._sqs.sendMessage.bind(this._sqs));
 
+    console.log("platform code",{queueUrl, message});
     await sendMessage({
       // Remove DelaySeconds parameter and value for FIFO queues
       DelaySeconds: 10,
