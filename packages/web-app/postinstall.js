ed02ba018564e82cad946a4a8ea3fb1b22c80983
Iago Soriano <iago.monteiro@usp.br>
2022-10-16 14:12:18 -0300

    postinstall moved

---
+++ packages/web-app/postinstall.js
@@ -0,0 +1,6 @@
+const fse = require('fs-extra');
+const path = require('path');
+const topDir = __dirname;
+const webAppPublicTinyMce = path.join(topDir, 'public', 'tinymce')
+fse.emptyDirSync(webAppPublicTinyMce);
+fse.copySync(path.join(topDir, '..','..','node_modules', 'tinymce'), webAppPublicTinyMce, { overwrite: true });
