638fb8a6f184c8a5ab9c98bf43620be4cb9f45c5
Iago Soriano <iago.monteiro@usp.br>
2022-10-26 20:28:40 -0300

    accordion refactored

---
+++ packages/web-app/src/atomic/atoms/accordion/index.tsx
@@ -0,0 +1,17 @@
+import BootstrapAccordion from 'react-bootstrap/Accordion';
+
+export const Accordion = () => {}
+
+const Wrapper = ({ children }) => (
+    <BootstrapAccordion defaultActiveKey={"0"} alwaysOpen> 
+        {children}
+    </BootstrapAccordion>
+);
+Accordion.Wrapper = Wrapper;
+
+const Item = ({ children, eventKey }) => (
+    <BootstrapAccordion.Item eventKey={eventKey}> 
+        {children}
+    </BootstrapAccordion.Item>
+);
+Accordion.Item = Item;
--- packages/web-app/src/atomic/atoms/index.ts
+++ packages/web-app/src/atomic/atoms/index.ts
@@ -1,3 +1,4 @@
+export * from './accordion';
 export * from './buttons';
 export * from './drawer-menu';
 export * from './error-loading-data';
