cacb2d044a8fb1af374f5de1f45442a2149b7462
Iago Soriano <iago.monteiro@usp.br>
2022-11-18 13:36:10 -0300

    staging env works

--- infra/environment/set-desired-count.sh
+++ infra/environment/set-desired-count.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-aws ecs update-service --cluster language-app-ecs-cluster --service auth-web-api --desired-count $1 > /dev/null &
-aws ecs update-service --cluster language-app-ecs-cluster --service web-api --desired-count $1 > /dev/null &
+aws ecs update-service --cluster language-app-ecs-cluster-staging --service auth-web-api-staging --desired-count $1 > /dev/null &
+aws ecs update-service --cluster language-app-ecs-cluster-production --service auth-web-api-production --desired-count $1 > /dev/null &
+aws ecs update-service --cluster language-app-ecs-cluster-staging --service web-api-staging --desired-count $1 > /dev/null &
+aws ecs update-service --cluster language-app-ecs-cluster-production --service web-api-production --desired-count $1 > /dev/null &
+
