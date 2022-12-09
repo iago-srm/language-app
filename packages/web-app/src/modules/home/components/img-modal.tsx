cc56cb2815c774bc8586e3d0c11a66b89036cb1d
Iago Soriano <iago.monteiro@usp.br>
2022-12-09 15:21:35 -0300

    home page modal height corrected

--- packages/web-app/src/modules/home/components/img-modal.tsx
+++ packages/web-app/src/modules/home/components/img-modal.tsx
@@ -4,7 +4,7 @@ export const ImgModal = ({ src, onClose, descr }) => {
   return (
     <Modal onClose={onClose}>
       <div style={{ textAlign: "center" }}>
-        <img src={src} width={"100%"} />
+        <img src={src} style={{ maxHeight: "80vh", width: "100%" }} />
         <p style={{ paddingTop: 20 }}>{descr}</p>
       </div>
     </Modal>
